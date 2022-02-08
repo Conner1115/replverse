c9587a54ff42c247724b273e49833b0b6ff58065
IroncladDev <connerow1115@gmail.com>
2022-02-08 21:35:36 +0000

    credit

--- pages/about.js
+++ pages/about.js
@@ -20,7 +20,7 @@ export default function About(){
 
         <p>One of the main goals of using the same palette is to give users a simillar feel to replit and make this appear to be attached to the real thing.</p>
 
-        <p>Huge thanks to <a href="https://replit.com/@JDOG787" target="_blank" rel="noreferrer">JDOG787</a> for coming up with the name Replverse and <a href="https://replit.com/@19wintersp" target="_blank" rel="noreferrer">19wintersp</a> for the logo and some of the badges.  Also, the hugest shout-out goes to <a href="https://replit.com/@Bookie0" target="_blank" rel="noreferrer">Bookie0</a> for providing the best design feedback, or Replverse would have ended up looking way different (in a not-so-good way).</p>
+        <p>Huge thanks to <a href="https://replit.com/@JDOG787" target="_blank" rel="noreferrer">JDOG787</a> for coming up with the name Replverse and <a href="https://replit.com/@19wintersp" target="_blank" rel="noreferrer">19wintersp</a> for the logo and some of the badges.  Also, the hugest shout-out goes to <a href="https://replit.com/@Bookie0" target="_blank" rel="noreferrer">Bookie0</a> for providing the best design feedback, or Replverse would have ended up looking way different (in a not-so-good way).  Also, thanks to <a href="https://replit.com/@retronbv" target="_blank" rel="noreferrer">RetronBV</a> for being an amazing beta tester for the chat!</p>
 
         <p>Have fun, enjoy, and behave yourself.  We&apos;re counting on you to make replverse a reality.</p>
       </div>
